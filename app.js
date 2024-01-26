cf1d5cf9ecd2091047123c518326341e0a9bb942
Maaem <maeliss.m98@gmail.com>
2024-01-26 10:17:42 +0100

    modif js affiche img

--- app.js
+++ app.js
@@ -9,20 +9,20 @@ questions.forEach((question) => {
 
 // Modifier l'image
 
-//  img_1 et img_2 = chemin img
-const img_1 = "assets/images/icon-plus.svg";
-const img_2 = "assets/images/icon-minus.svg";
+//  img_1 et img_2 = chemin vers les images
+const img_1 = "./assets/images/icon-plus.png";
+const img_2 = "./assets/images/icon-minus.svg";
 
 // Par défaut, chaque image commence avec img_1
 let currentImage = img_1;
 
-// Sélectionnez toutes vos images et ajoutez un gestionnaire d'événements à chacune d'elles
+// Sélectionne toutes les balises img et ajoute un gestionnaire d'événements à chacune d'elles
 const images = document.querySelectorAll('img');
 
 images.forEach((image) => {
     // Ajoutez un gestionnaire d'événements pour le clic sur chaque image
     image.addEventListener("click", () => {
-        // Vérifiez l'état actuel de l'image et changez-la en conséquence
+        // Vérification de l'état actuel de l'image et change en conséquence
         if (currentImage === img_1) {
             image.src = img_2;
             currentImage = img_2;
